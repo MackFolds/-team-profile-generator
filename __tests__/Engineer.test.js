4824c8c837f2696ccbf83dd91ba703b357d280a1
Mack Folds <treyfolds93@gmail.com>
2021-05-16 13:52:05 -0600

    add engineer.test.js test

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,25 @@
+const Employee = require('../lib/Employee');
+const Engineer = require('../lib/Engineer');
+
+// mock's an Employee
+jest.mock('../lib/Employee');
+
+test('tests engineer object creation', () => {
+    const engineer = new Engineer()
+
+    expect(engineer.name).toBe('Mack');
+    expect(engineer.id).toBe(321);
+    expect(engineer.email).toBe('email@email.com');
+})
+
+test("get engineer's role", () => {
+    const engineer = new Engineer();
+    engineer.getRole()
+    expect(engineer.getRole()).toEqual(expect.stringContaining('Engineer'));
+});
+
+test("get engineer's github", () => {
+    const engineer = new Engineer();
+
+    expect(engineer.getGitHub('github.com/repo-name')).toEqual(expect.any(String));
+})
